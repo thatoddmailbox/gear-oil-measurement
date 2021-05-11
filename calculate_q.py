bba2f3af1d15fdee8651edf8fff5e1caf1a230d2
Alex Studer <alex@studer.dev>
2021-05-10 21:38:37 -0400

    can get s11 at closest point to manually set resonance

--- calculate_q.py
+++ calculate_q.py
@@ -1,15 +1,24 @@
-### setup ###
 import nanovnacmd as nv
 import numpy as np
 import matplotlib.pyplot as plt
 
+from typing import List
+
 from NanoVNASaver.Calibration import Calibration
+from NanoVNASaver.RFTools import Datapoint
 from NanoVNASaver.Touchstone import Options, Touchstone
+
+def closest_point(data: List[Datapoint], freq: int) -> Datapoint:
+	closest = None
+	closest_dist = None
+	for point in data:
+		if not closest_dist or abs(point.freq - freq) < closest_dist:
+			closest = point
+			closest_dist = abs(point.freq - freq)
+	return closest
 
 ts = Touchstone("./test1_good.s1p")
 ts.load()
-
-print(ts.opts)
 
 f = np.array([d.freq for d in ts.s11data])
 s11 = np.array([d.z for d in ts.s11data])
@@ -19,13 +28,14 @@ plt.plot(f/1e9,20*np.log10(np.abs(s11)),label='S11')
 plt.xlabel('Frequency (GHz)')
 plt.ylabel('S parameter (dB)')
 plt.ylim(-80,5)
-plt.show()
+# plt.show()
 
 plt.plot(f/1e9,np.angle(s11),label='S11')
 # plt.plot(f/1e9,np.angle(s21),label='S21')
 plt.xlabel('Frequency (GHz)')
 plt.ylabel('Phase (rad)')
 plt.ylim(-np.pi,np.pi)
-plt.show()
+# plt.show()
 
-resonant_frequency = 2.879
+resonant_frequency = 2.879 * 1e9
+resonant_s11 = closest_point(ts.s11data, resonant_frequency)
