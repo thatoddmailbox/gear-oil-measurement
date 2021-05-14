b12001be32a652b6edecee808b546b11f6175399
Alex Studer <alex@studer.dev>
2021-05-13 20:30:13 -0400

    calculate_q: disable resonance restrictions

--- calculate_q.py
+++ calculate_q.py
@@ -152,21 +152,21 @@ resonant_s11 = closest_point(ts.s11data, resonant_frequency)
 resonant_gain = resonant_s11.gain
 print("resonant gain:", resonant_gain)
 
-half_power_bandwidth = calculate_half_power_bandwidth(ts, resonant_gain)
+half_power_bandwidth = calculate_half_power_bandwidth(ts, resonant_frequency, resonant_gain)
 
 # Q_loaded = half power bandwidth
 q_loaded = resonant_frequency / half_power_bandwidth
 print("q_loaded:", q_loaded)
 
-resonance_band_from = 2.500 * 1e9
-resonance_band_to = 2.870 * 1e9
+# resonance_band_from = 2.500 * 1e9
+# resonance_band_to = 2.870 * 1e9
 # https://media.proquest.com/media/hms/ORIG/1/7ZvgC?hl=&cit%3Aauth=Shahid%2C+S%3BBall%2C+J+A+R%3BWells%2C+C+G%3BWen%2C+P&cit%3Atitle=Reflection+type+Q-factor+measurement+using+standard+least+squares+methods&cit%3Apub=IEE+proceedings.+Microwaves%2C+antennas+and+propagation.&cit%3Avol=5&cit%3Aiss=4&cit%3Apg=426&cit%3Adate=Mar+2011&ic=true&cit%3Aprod=Advanced+Technologies+%26+Aerospace+Collection&_a=ChgyMDIxMDUxMDIwMjIwMjQzNjo2Nzk2MDESBjEwMDg2NRoKT05FX1NFQVJDSCIMMTguMTguMjM5LjE2KgcxOTM2MzYxMgoxNjM2NzAyNTA1Og1Eb2N1bWVudEltYWdlQgEwUgZPbmxpbmVaAkZUYgNQRlRqCjIwMTEvMDMvMDFyCjIwMTEvMDMvMzF6AIIBKVAtMTAwNzg1Mi0xMjQ5Mi1DVVNUT01FUi0xMDAwMDIzMy01MDQ4NjM5kgEGT25saW5lygFMTW96aWxsYS81LjAgKFgxMTsgVWJ1bnR1OyBMaW51eCB4ODZfNjQ7IHJ2Ojg3LjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvODcuMNIBElNjaG9sYXJseSBKb3VybmFsc5oCB1ByZVBhaWSqAi5PUzpFTVMtRG9jVmlld1BkZlVybFNlcnZpY2UtZ2V0TWVkaWFVcmxGb3JJdGVtygIPQXJ0aWNsZXxGZWF0dXJl0gIBWeICAU7yAgD6AgFOggMDV2ViigMcQ0lEOjIwMjEwNTEwMjAyMjAyNDMyOjg1Mzg4Mg%3D%3D&_s=3xxU4Nz07bp9dqMrWeemRkVM%2FxI%3D
 # now that we have q, find gamma L, the closest to origin
 gamma_L = None
 gamma_L_dist = None
 for p in ts.s11data:
-	if p.freq < resonance_band_from or p.freq > resonance_band_to:
-		continue
+	# if p.freq < resonance_band_from or p.freq > resonance_band_to:
+	# 	continue
 	gamma = p.z
 	distance = abs(gamma)
 	if not gamma_L_dist or distance < gamma_L_dist:
